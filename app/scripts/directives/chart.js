c7a3a0f21578aedd34a7cec18206cf1e618c6eaa
dollaruw <mail@jamesmakes.com>
2014-07-01 16:12:58 -0400

    still just circles actually updates

--- app/scripts/directives/chart.js
+++ app/scripts/directives/chart.js
@@ -23,19 +23,21 @@ angular.module('anExApp')
 						.attr('width',width)
 						.attr('height',height);
 
-			var drawChart = function() {
-				svg.selectAll('circle')
-					.data(scope.display)
-					.enter()
-					.append('circle')
+			var drawChart = function(data) {
+				var circle = svg.selectAll('circle')
+								.data(data);
+
+				circle.enter().append('circle')
 					.attr('cx',function(){ return Math.random() * width; })
 					.attr('cy',function(){ return Math.random() * height; })
-					.attr('r',function(d){ return Math.sqrt(d.POPESTIMATE2013)/1000; });
+					.attr('r',function(d){ return Math.sqrt(d.POPESTIMATE2013)/100; });
+
+				circle.exit().remove();
 			}
 
 			scope.$watch('display',function(newVal,oldVal){
 				if (!_.isEqual(newVal,oldVal)) {
-					drawChart();
+					drawChart(newVal);
 				}
 			});
 
